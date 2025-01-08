d0f87a69b68f4f2cb505a6d072bace655203291f
Viet Hoang <gvhoang30112004@gmail.com>
2025-01-08 20:24:33 +0700

    FINISH JSF WEEK 1 PART 2 BAI 2 UPDATE FIX NAME

--- Tuan1/bai2-part2.js
+++ Tuan1/bai2-part2.js
@@ -10,6 +10,8 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
+
+
 console.log('Hóa đơn:', bills);
 console.log('Tiền tip:', tips);
 console.log('Tổng cộng:', total);
