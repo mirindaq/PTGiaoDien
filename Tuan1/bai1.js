fba45e7718819660ba2a3463262868d2105b79bb
Viet Hoang <gvhoang30112004@gmail.com>
2025-01-08 20:13:51 +0700

    FINISH JSF WEEK 1 PART 1 BAI 1

---
+++ Tuan1/bai1.js
@@ -0,0 +1,32 @@
+const weightMark = 78
+const heightMart = 1.69
+const weightJohn = 92
+const heightJohn = 1.95
+
+const weightMark2 = 95
+const heightMart2 = 1.88
+const weightJohn2 = 85
+const heightJohn2 = 1.76
+
+const calBMI = (weight, height) => (
+    weight / (height * height)
+)
+
+
+const BMIMark = calBMI(weightMark, heightMart);
+const BMIJohn = calBMI(weightJohn, heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+const BMIMark2 = calBMI(weightMark2, heightMart2);
+const BMIJohn2 = calBMI(weightJohn2, heightJohn2);
+
+const markHigherBMI2 = BMIMark2 > BMIJohn2;
+
+console.log('BMI của Mark test 1:', BMIMark.toFixed(2));
+console.log('BMI của John test 1:', BMIJohn.toFixed(2));
+
+
+console.log('BMI của Mark test 2:', BMIMark2.toFixed(2));
+console.log('BMI của John test 2:', BMIJohn2.toFixed(2));
+
+
